51dbd182d54d4a4691415a50563671009d6db4b8
andy <andy.wang@webcomm.com.tw>
2022-12-01 09:58:57 +0800

    調整使用方式

--- js/relation-graph.js
+++ js/relation-graph.js
@@ -23,16 +23,16 @@ function draw(graphObj, visibility) {
                 div.textContent = `${key}:${value}`;
             }
         })
-        .linkWidth(3)
         .nodeAutoColorBy('controller')
+        .nodeLabel('memo')
         .nodeCanvasObject((node, ctx, globalScale) => {
             const label = node.id;
-            const fontSize = 12/globalScale;
+            const fontSize = 16/globalScale;
             ctx.font = `${fontSize}px Sans-Serif`;
             const textWidth = ctx.measureText(label).width;
             const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
 
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            ctx.fillStyle = 'rgba(255, 255, 255, 1)';
             ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
 
             ctx.textAlign = 'center';
@@ -46,7 +46,9 @@ function draw(graphObj, visibility) {
             ctx.fillStyle = color;
             const bckgDimensions = node.__bckgDimensions;
             bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-        });
+        })
+        .linkWidth(3)
+        .linkDirectionalArrowLength(8);
 
     adjustGraphSize(Graph)
     window.addEventListener('resize', () => {
